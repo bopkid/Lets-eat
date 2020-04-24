c81b0e5c758f2be6fbade7eb3f53ec97627eed21
lucky-ly <luckster8089@gmail.com>
2020-04-24 12:18:25 -0700

    Boilerplate for Let's Eat

---
+++ server.js
@@ -0,0 +1,39 @@
+// Bring in express
+const express = require('express')
+
+// Invoke Express
+const app = express();
+
+// Express Session
+const session = require('express-session')
+
+// Bring in body-parser
+const bodyParser = require('body-parser');
+
+// Bring in Method-Override
+const methodOverride = require('method-override');
+
+// Port Activation
+const port = process.env.PORT || 4000;
+
+// Set View Engine
+app.set('view engine', 'ejs')
+
+// ---------------------------------------- MIDDLEWARE-----------------
+// Express Session
+app.use(session({
+    secret: 'jfiewo;ajio23r243f4igjowssieowi;oiwajawqwbbcd', 
+    resave: false, 
+    saveUninitialized: false, 
+}))
+
+// Init body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Method Override
+app.use(methodOverride('_method'));
+
+// ------------------------------------------ SERVER LISTENER -------------
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));

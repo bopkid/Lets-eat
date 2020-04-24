1337951e4f674c2b4a1e69b36e2fc439fb0aa11f
alwong <alwong125@gmail.com>
2020-04-24 13:43:09 -0700

    added mongodb connection

---
+++ models/index.js
@@ -0,0 +1,18 @@
+const mongoose =require('mongoose');
+
+const connectionString = 'mongodb://localhost:27017/Lets-eat'
+const configOption ={
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+}
+
+mongoose.connect(connectionString,configOption)
+    .then(()=>{console.log('MongoDB conntected sucessfully')})
+    .catch((err)=>{console.log(`MongoDB connection error: ${err}`)})
+
+
+module.exports = {
+    Recipe: require('./Recipe')
+}

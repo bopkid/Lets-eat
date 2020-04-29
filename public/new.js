6a04d92fa0ec0bbd0dd0a40a90d115d36c85555b
alwong <alwong125@gmail.com>
2020-04-29 16:19:45 -0700

    added new way to display the ingredeints and directions

--- public/new.js
+++ public/new.js
@@ -1,27 +1,37 @@
 
 console.log('here');
 const ingredients = document.querySelector('.ingredients');
-const dierections = document.querySelector('.dierections')
+const direction = document.querySelector('.directions')
 
 document.querySelector('.new-ingredient-btn').addEventListener('click',(eve)=>{
+
     eve.preventDefault();
     const newIngredent  = document.createElement("textArea");
+
     newIngredent.className = 'new-ingredient';
-    newIngredent.setAttribute('rows', 1)
-    newIngredent.setAttribute('cols', 25)
+    newIngredent.setAttribute('rows', 1);
+    newIngredent.setAttribute('cols', 25);
+    newIngredent.setAttribute('cols', 25);
+    newIngredent.setAttribute('id','ingredients' );
+    newIngredent.setAttribute('name',  "ingredients")
+
     ingredients.appendChild(newIngredent);
 
     console.log('lciked');
    
 })
 
-document.querySelector('.new-dierections-btn').addEventListener('click',(e)=>{
+document.querySelector('.new-direction-btn').addEventListener('click',(e)=>{
+
     e.preventDefault();
+
     console.log('herer')
     const newDierection = document.createElement('textarea');
 
-    newDierection.className = 'new-dierections';
+    newDierection.className = 'new-direction';
     newDierection.setAttribute('rows', 5);
     newDierection.setAttribute('cols', 70);
-    dierections.appendChild(newDierection);
+    newDierection.setAttribute('id','direction' );    
+    newDierection.setAttribute('name',  "direction");
+    direction.appendChild(newDierection);
 })
